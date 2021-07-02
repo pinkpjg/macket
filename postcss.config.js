7ff9461ddbf63adc79d4dc24f3e34f773b7e5582
1225303595@qq.com <9284753+front-end-xiaobaipeng@user.noreply.gitee.com>
2021-07-02 14:29:05 +0800

    购物车完成

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,14 @@ module.exports = {
       selectorBlackList:['ignore','tab-bar','tab-bar-item'],//制定不需要转换的类
       minPixelValue:1,//小于或等于’1px‘不转换成视口单位
       mediaQuery:false,//允许在媒体查询中转换'px'
-      // exclude:['TabBarItem.vue']
+      exclude:[/TabBar/]
     },
   }
 }
+
+//1.在js中使用正则表达式：用//把正则相关规则包起来
+//2.exclude中存放的元素必须是正则表达式
+//3.按照排除的文件写对应的正则:
+//正则的规则：
+//^abc:表示匹配的内容，必须以什么内容开头（以abc为开头）
+//abc$:表示匹配的内容，必须以什么内容结尾（以adc结尾）
